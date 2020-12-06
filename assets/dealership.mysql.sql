8e43c358b47dfb852f3da6e7dd8c993b1ab25e72
Keith Walsh <26302786+Walsh-Media@users.noreply.github.com>
2020-12-06 23:35:11 +0000

    Update dealership.mysql.sql

--- assets/dealership.mysql.sql
+++ assets/dealership.mysql.sql
@@ -1,127 +1,166 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema dealership
--- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `dealership` ;
-
--- -----------------------------------------------------
--- Schema dealership
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `dealership` DEFAULT CHARACTER SET utf8 ;
-USE `dealership` ;
-
--- -----------------------------------------------------
--- Table `dealership`.`Color`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dealership`.`Color` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`Id`),
-  UNIQUE INDEX `Name_UNIQUE` (`Name` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `dealership`.`CarMaker`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dealership`.`CarMaker` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`Id`),
-  UNIQUE INDEX `Name_UNIQUE` (`Name` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `dealership`.`BodyType`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dealership`.`BodyType` (
-  `Id` INT NOT NULL,
-  `Name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`Id`),
-  UNIQUE INDEX `Name_UNIQUE` (`Name` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `dealership`.`CarModel`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `dealership`.`CarModel` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
-  `Color` INT NOT NULL,
-  `CarMaker` INT NOT NULL,
-  `Price` DOUBLE NULL,
-  `Name` VARCHAR(45) NULL,
-  `BodyType` INT NOT NULL,
-  `EngineLitres` DOUBLE NULL,
-  PRIMARY KEY (`Id`),
-  INDEX `fk_Car_Color_idx` (`Color` ASC) VISIBLE,
-  INDEX `fk_CarModel_CarMaker1_idx` (`CarMaker` ASC) VISIBLE,
-  INDEX `fk_CarModel_BodyType1_idx` (`BodyType` ASC) VISIBLE,
-  CONSTRAINT `fk_Car_Color`
-    FOREIGN KEY (`Color`)
-    REFERENCES `dealership`.`Color` (`Id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_CarModel_CarMaker1`
-    FOREIGN KEY (`CarMaker`)
-    REFERENCES `dealership`.`CarMaker` (`Id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_CarModel_BodyType1`
-    FOREIGN KEY (`BodyType`)
-    REFERENCES `dealership`.`BodyType` (`Id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
--- -----------------------------------------------------
--- Data for table `dealership`.`Color`
--- -----------------------------------------------------
-START TRANSACTION;
+CREATE DATABASE  IF NOT EXISTS `dealership` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `dealership`;
-INSERT INTO `dealership`.`Color` (`Id`, `Name`) VALUES (1, 'Blue');
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dealership
+-- ------------------------------------------------------
+-- Server version	8.0.22
 
-COMMIT;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `bodytype`
+--
 
--- -----------------------------------------------------
--- Data for table `dealership`.`CarMaker`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `dealership`;
-INSERT INTO `dealership`.`CarMaker` (`Id`, `Name`) VALUES (1, 'BMW');
+DROP TABLE IF EXISTS `bodytype`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bodytype` (
+  `Id` int NOT NULL,
+  `Name` varchar(45) NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Name_UNIQUE` (`Name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-COMMIT;
+--
+-- Dumping data for table `bodytype`
+--
 
+LOCK TABLES `bodytype` WRITE;
+/*!40000 ALTER TABLE `bodytype` DISABLE KEYS */;
+INSERT INTO `bodytype` VALUES (1,'Hatchback');
+/*!40000 ALTER TABLE `bodytype` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Data for table `dealership`.`BodyType`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `dealership`;
-INSERT INTO `dealership`.`BodyType` (`Id`, `Name`) VALUES (1, 'Hatchback');
+--
+-- Table structure for table `car`
+--
 
-COMMIT;
+DROP TABLE IF EXISTS `car`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `car` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `RegistrationNumber` varchar(45) NOT NULL,
+  `Value` double NOT NULL,
+  `OwnerName` varchar(45) NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `RegistrationNumber_UNIQUE` (`RegistrationNumber`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `car`
+--
 
--- -----------------------------------------------------
--- Data for table `dealership`.`CarModel`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `dealership`;
-INSERT INTO `dealership`.`CarModel` (`Id`, `Color`, `CarMaker`, `Price`, `Name`, `BodyType`, `EngineLitres`) VALUES (1, 1, 1, 35000, '3 Series', 1, 2);
-INSERT INTO `dealership`.`CarModel` (`Id`, `Color`, `CarMaker`, `Price`, `Name`, `BodyType`, `EngineLitres`) VALUES (2, 1, 1, 25000, '2 Series', 1, 1.8);
+LOCK TABLES `car` WRITE;
+/*!40000 ALTER TABLE `car` DISABLE KEYS */;
+INSERT INTO `car` VALUES (1,'01-LK-1234',99999,'John Doe'),(2,'02-LK-2345',99998,'Jan Doe');
+/*!40000 ALTER TABLE `car` ENABLE KEYS */;
+UNLOCK TABLES;
 
-COMMIT;
+--
+-- Table structure for table `carmaker`
+--
 
+DROP TABLE IF EXISTS `carmaker`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `carmaker` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(45) NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Name_UNIQUE` (`Name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `carmaker`
+--
+
+LOCK TABLES `carmaker` WRITE;
+/*!40000 ALTER TABLE `carmaker` DISABLE KEYS */;
+INSERT INTO `carmaker` VALUES (1,'BMW');
+/*!40000 ALTER TABLE `carmaker` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `carmodel`
+--
+
+DROP TABLE IF EXISTS `carmodel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `carmodel` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `Color` int NOT NULL,
+  `CarMaker` int NOT NULL,
+  `Price` double DEFAULT NULL,
+  `Name` varchar(45) DEFAULT NULL,
+  `BodyType` int NOT NULL,
+  `EngineLitres` double DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `fk_Car_Color_idx` (`Color`),
+  KEY `fk_CarModel_CarMaker1_idx` (`CarMaker`),
+  KEY `fk_CarModel_BodyType1_idx` (`BodyType`),
+  CONSTRAINT `fk_Car_Color` FOREIGN KEY (`Color`) REFERENCES `color` (`Id`),
+  CONSTRAINT `fk_CarModel_BodyType1` FOREIGN KEY (`BodyType`) REFERENCES `bodytype` (`Id`),
+  CONSTRAINT `fk_CarModel_CarMaker1` FOREIGN KEY (`CarMaker`) REFERENCES `carmaker` (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `carmodel`
+--
+
+LOCK TABLES `carmodel` WRITE;
+/*!40000 ALTER TABLE `carmodel` DISABLE KEYS */;
+INSERT INTO `carmodel` VALUES (1,1,1,35000,'3 Series',1,2),(2,1,1,25000,'2 Series',1,1.8);
+/*!40000 ALTER TABLE `carmodel` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `color`
+--
+
+DROP TABLE IF EXISTS `color`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `color` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(45) NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Name_UNIQUE` (`Name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `color`
+--
+
+LOCK TABLES `color` WRITE;
+/*!40000 ALTER TABLE `color` DISABLE KEYS */;
+INSERT INTO `color` VALUES (1,'Blue');
+/*!40000 ALTER TABLE `color` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-12-06 23:35:00
